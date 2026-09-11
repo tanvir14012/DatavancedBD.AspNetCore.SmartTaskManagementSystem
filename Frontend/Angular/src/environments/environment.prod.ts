123d195efe50b5cafc328b07486042bb7fd53929
tanvir14012 <tanvir14012@gmail.com>
2026-09-11 20:06:36 +0600

    DevOps: Add production CI-CD pipeline (Windows IIS)

--- Frontend/Angular/src/environments/environment.prod.ts
+++ Frontend/Angular/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  //apiBaseUrl: '/services/api' // For IIS (services as application alias)
-  apiBaseUrl: '/api' // For nginx
+  apiBaseUrl: '/services/api'
 };
